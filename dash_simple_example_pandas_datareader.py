ff1b1cf651c244e2e724bef9c83cb567e830e569
Gareth Ellis <gareth.ellis0@gmail.com>
2020-03-22 12:40:29 -0700

    Created Super Simple Viewer For CSV Data Using Dash

---
+++ dash_simple_example_pandas_datareader.py
@@ -0,0 +1,57 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+from pandas import *
+from datetime import datetime as dt
+
+app = dash.Dash('Hello World')
+
+app.layout = html.Div([
+    #dcc.Dropdown(
+    #    id='my-dropdown',
+    #    options=[
+    #        {'label': 'Coke', 'value': 'COKE'},
+    #        {'label': 'Tesla', 'value': 'TSLA'},
+    #        {'label': 'Apple', 'value': 'AAPL'}
+    #    ],
+    #    value='COKE'
+    #),
+
+    dcc.Dropdown(id='dropdown'),
+    dcc.Graph(
+        id='my-graph',
+        #figure={
+        #    'data': [{
+        #        'x': pandas.read_csv("data.csv")["time"],
+        #        'y': pandas.read_csv("data.csv")["weight_grams"],
+        #        }],
+        #    'layout': {
+        #        'title': 'Dash Data Visualization'
+        #    }
+        #}
+    )], style={'width': '500'})
+
+@app.callback(Output('my-graph', 'figure'),  [Input('dropdown', 'value')])
+def update_graph(dropdown_value):
+    df = pandas.read_csv("data.csv")
+    print(df)
+    #df = web.DataReader(
+    #    selected_dropdown_value,
+    #    'google',
+    #    dt(2017, 1, 1),
+    #    dt.now()
+    #)
+    return {
+        'data': [{
+            'x': df["time"],
+            'y': df["weight_grams"],
+        }],
+        'layout': {'margin': {'l': 40, 'r': 0, 't': 20, 'b': 30}}
+    }
+
+app.css.append_css({'external_url': 'https://codepen.io/chriddyp/pen/bWLwgP.css'})
+
+if __name__ == '__main__':
+    app.run_server()
